6607964c2931779654f0368ac8f1eec69806c0e8
mapik <mapik@mapik.net>
2021-05-21 10:01:02 +0200

    add radius defenition

--- config/app_local.example.php
+++ config/app_local.example.php
@@ -83,6 +83,30 @@ return [
              */
             'url' => env('DATABASE_RUIAN_URL', null),
         ],
+        'radius' => [
+            'host' => 'localhost',
+            /*
+             * CakePHP will use the default DB port based on the driver selected
+             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
+             * the following line and set the port accordingly
+             */
+            //'port' => 'non_standard_port_number',
+
+            'username' => 'my_app',
+            'password' => 'secret',
+
+            'database' => 'my_app',
+            /*
+             * If not using the default 'public' schema with the PostgreSQL driver
+             * set it here.
+             */
+            //'schema' => 'myapp',
+
+            /*
+             * You can use a DSN string to set the entire configuration
+             */
+            'url' => env('DATABASE_RADIUS_URL', null),
+        ],
 
         /*
          * The test connection is used during the test suite.
