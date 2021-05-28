c4b801f0f137cbef68d9ba144cd17a79cf815ad8
mapik <mapik@mapik.net>
2021-05-28 12:50:00 +0200

    RADIUS - users - redirect to view after add

--- plugins/RADIUS/src/Controller/UsersController.php
+++ plugins/RADIUS/src/Controller/UsersController.php
@@ -89,7 +89,7 @@ class UsersController extends AppController
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
+                return $this->redirect(['action' => 'view', $user->id]);
             }
             $this->Flash->error(__('The user could not be saved. Please, try again.'));
         }
