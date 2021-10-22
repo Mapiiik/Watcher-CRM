be1a7acd2136b377165fb5edb5a19a8fe13704b5
mapik <mapik@mapik.net>
2021-10-22 14:09:53 +0200

    Bookkeeping - verification function for comparision of generated invoices with CSV data

--- plugins/BookkeepingPohoda/src/Controller/InvoicesController.php
+++ plugins/BookkeepingPohoda/src/Controller/InvoicesController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace BookkeepingPohoda\Controller;
 
 use BookkeepingPohoda\Controller\AppController;
+use Cake\I18n\FrozenDate;
+use Cake\ORM\Query;
 
 /**
  * Invoices Controller
@@ -118,17 +120,143 @@ class InvoicesController extends AppController
     public function generate()
     {
         $this->Customers = $this->getTableLocator()->get('Customers');
+
+        $taxes = $this->Customers->Taxes->find('list', ['order' => 'name']);
         
         $query = $this->request->getQuery();
         
-        $taxes = $this->Customers->Taxes->find('list', ['order' => 'name']);
+        if ($this->request->is(['post'])) {
+            $data = $this->request->getData();
+            
+            // DO THIS BETTER - REVERSE CHARGE
+            if ($data['tax_rate_id'] == 5) {
+                $reverse_charge = true;
+                $innerfix = "8";
+            } else {
+                $reverse_charge = false;
+                $innerfix = "9";
+            }            
+            
+            $invoiced_month = new FrozenDate($this->request->getData('invoiced_month'));
+            
+            $customers = $this->Customers
+                    ->find()
+                    ->order('Customers.id')
+                    ->where(['Customers.taxe_id' => $data['tax_rate_id']])
+                    ->contain('Addresses')
+                    ->contain('Contracts', function (Query $q) use ($invoiced_month) {
+                        return $q
+                                ->order('Contracts.id')
+                                ->where([
+                                    'OR' => [
+                                        'Contracts.valid_from IS NULL',
+                                        'Contracts.valid_from <=' => $invoiced_month->lastOfMonth(), //last day of month
+                                    ],
+                                    'OR' => [
+                                        'Contracts.valid_until IS NULL',
+                                        'Contracts.valid_until >=' => $invoiced_month->firstOfMonth(), //first day of month
+                                    ],
+                                ])
+                                ->contain('Billings', function (Query $q) use ($invoiced_month) {
+                                    return $q
+                                            ->order('Billings.id')
+                                            ->where([
+                                                'Billings.active' => true,
+                                                'OR' => [
+                                                    'Billings.billing_from IS NULL',
+                                                    'Billings.billing_from <=' => $invoiced_month->lastOfMonth(), //last day of month
+                                                ],
+                                                'OR' => [
+                                                    'Billings.billing_until IS NULL',
+                                                    'Billings.billing_until >=' => $invoiced_month->firstOfMonth(), //first day of month
+                                                ],
+                                            ])
+                                            ->contain(['Services']);
+                                });
+                    });
+            
+            // verification data
+            if ($data['csv_for_verification']->getSize() > 0) {
+                $lines = explode(PHP_EOL, $data['csv_for_verification']->getStream()->getContents());
+                $verification_data = [];
+                
+                foreach ($lines as $line) {
+                    $parsed_line = explode(",", $line);
+                    
+                    if (!is_numeric(trim($parsed_line[0]))) continue;
+                    
+                    if (!isset($verification_data[trim($parsed_line[0])])) {
+                        $verification_data[trim($parsed_line[0])]['csv']['total'] = 0;
+                        $verification_data[trim($parsed_line[0])]['csv']['items'] = [];
+                    }
+                    
+                    $item = new \Cake\ORM\Entity;
+                    if (isset($parsed_line[1])) {
+                        $item->period_total = trim($parsed_line[1]);
+                    }
+                    if (isset($parsed_line[2])) {
+                        $item->name = trim($parsed_line[2]);
+                    }
+                    
+                    $verification_data[trim($parsed_line[0])]['csv']['total'] += $item->period_total;
+                    $verification_data[trim($parsed_line[0])]['csv']['items'][] = $item;
+                    
+                    unset($item);
+                    unset($parsed_line);
+                }
+                unset($lines);
+            }
 
-        if (isset($query['tax_rate_id'])) {
-            $tax_rate_id = $query['tax_rate_id'];
+            // billing data
+            $billing_data = [];
+
+            foreach ($customers as $customer) {
+                foreach ($customer->contracts as $contract) {
+                    foreach ($contract->billings as $billing) {
+                        $billing->period_total = $billing->periodTotal($invoiced_month->firstOfMonth(), $invoiced_month->lastOfMonth());
+                        
+                        if (!isset($billing_data[$customer->number])) {
+                            $billing_data[$customer->number]['total'] = 0;
+                            $billing_data[$customer->number]['items'] = [];
+                        }
+
+                        $billing_data[$customer->number]['total'] += $billing->period_total;
+                        $billing_data[$customer->number]['items'][] = $billing;
+                        
+                        unset($total);
+                    }
+                }
+
+                // remove invoices with zero price
+                if (isset($billing_data[$customer->number]) && $billing_data[$customer->number]['total'] == 0) {
+                    unset($billing_data[$customer->number]);
+                }
+
+                // compare invoice with verification data if loaded and billing data available
+                if (isset($verification_data) && isset($billing_data[$customer->number])) {
+                    if (isset($verification_data[$customer->number])) {
+                        if ($verification_data[$customer->number]['csv']['total'] == $billing_data[$customer->number]['total']) {
+                            // remove from verification if OK
+                            unset($verification_data[$customer->number]);
+                        } else {
+                            // add billing data to verification if not OK
+                            $verification_data[$customer->number]['crm'] = $billing_data[$customer->number];
+                        }
+                    } else {
+                        if (isset($billing_data[$customer->number])) {
+                            // create missing verification data if there are billing data
+                            $verification_data[$customer->number]['crm'] = $billing_data[$customer->number];
+                        }
+                    }
+                }
+            }
+            
+            if (isset($verification_data) && !empty($verification_data)) {
+                $this->set('verification_data', $verification_data);
+            }
         }
-
+        
         if ($this->request->getParam('_ext') === 'dbf') {
-            debug($this->request->getData());
 /*            switch ($type) {
             case 'gdpr-new':
             case 'gdpr-change':
@@ -140,7 +268,7 @@ class InvoicesController extends AppController
             }
 */
                 $this->Flash->error(__('Invalid type of document requested.'));
-                return $this->redirect(['action' => 'generate', '?' => $this->request->getData()]);
+                return $this->redirect(['action' => 'generate', '?' => $this->request->getQuery()]);
 /*            
             // filter and split billings
             $contract->individual_billings = [];
--- plugins/BookkeepingPohoda/templates/Invoices/generate.php
+++ plugins/BookkeepingPohoda/templates/Invoices/generate.php
@@ -14,10 +14,9 @@
         <div class="invoices form content">
             <?= $this->Form->create(null, [
                 'type' => 'file',
-                'valueSources' => ['query', 'context'],
+                'valueSources' => ['data', 'query'],
                 'url' => [
                     'action' => 'generate',
-                    '_ext' => 'dbf'
                 ]
             ]) ?>
             <fieldset>
@@ -37,5 +36,69 @@
             <?= $this->Form->button(__('Submit')) ?>
             <?= $this->Form->end() ?>
         </div>
+        
+        <?php if (isset($verification_data)): ?>
+        <br />
+        <div class="table-responsive">
+            <table>
+                <thead>
+                    <tr>
+                        <th><?= __('Customer') ?></th>
+                        <th>CRM</th>
+                        <th>CSV</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($verification_data as $customer_number => $customer_comparision): ?>
+                    <tr>
+                        <td><?= h($customer_number) ?></td>
+                        <td>
+                            <?php if (isset($customer_comparision['crm'])): ?>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th><?= __('Name') ?></th>
+                                        <th><?= __('Price') ?></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($customer_comparision['crm']['items'] as $item): ?>
+                                    <tr>
+                                        <td><?= h($item->name) ?></td>
+                                        <td><?= $this->Number->currency($item->period_total) ?></td>
+                                    </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                            <?= __('Total') . ': ' . $this->Number->currency($customer_comparision['crm']['total']) ?>
+                            <?php endif; ?>
+                        </td>
+                        <td>
+                            <?php if (isset($customer_comparision['csv'])): ?>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th><?= __('Name') ?></th>
+                                        <th><?= __('Price') ?></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($customer_comparision['csv']['items'] as $item): ?>
+                                    <tr>
+                                        <td><?= h($item->name) ?></td>
+                                        <td><?= $this->Number->currency($item->period_total) ?></td>
+                                    </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                            <?= __('Total') . ': ' . $this->Number->currency($customer_comparision['csv']['total']) ?>
+                            <?php endif; ?>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        <?php endif; ?>
     </div>
 </div>
