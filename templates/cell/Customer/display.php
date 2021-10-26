9012c2673b67899f53c0f76b5b0fe72f10aeecd1
mapik <mapik@mapik.net>
2021-10-26 23:07:12 +0200

    Display - use new name getter for Contract

--- templates/cell/Customer/display.php
+++ templates/cell/Customer/display.php
@@ -8,11 +8,9 @@
         </div>
         <div class="nav-content-right">
             <?php foreach ($customer->contracts as $contract): ?>
-                <?= $this->AuthLink->link(
-                    $contract->number .
-                        ($contract->has('service_type') ? ' - ' . $contract->service_type->name : '') .
-                        ($contract->has('installation_address') ? ' - ' . $contract->installation_address->address : ''),
-                    ['plugin' => null, 'controller' => 'Contracts', 'action' => 'view', $contract->id], ['class' => 'button button-small' . (($contract->id == $this->request->getParam('contract_id')) ? ' button-selected' : '')])
+                <?= $this->AuthLink->link($contract->name,
+                        ['plugin' => null, 'controller' => 'Contracts', 'action' => 'view', $contract->id],
+                        ['class' => 'button button-small' . (($contract->id == $this->request->getParam('contract_id')) ? ' button-selected' : '')])
                 ?>
             <?php endforeach; ?>
         </div>
