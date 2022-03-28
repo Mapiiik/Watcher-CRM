93e7408492417b3cfb185ea9dfa94756b3c95459
Martin Patocka <mapik@mapik.net>
2022-03-28 10:21:48 +0000

    Migrations - add missing sessions table

---
+++ config/Migrations/20220328101609_CreateSessions.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreateSessions extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * @return void
+     */
+    public function change()
+    {
+        $this->table('sessions', ['id' => false, 'primary_key' => ['id']])
+            ->addColumn('id', 'string', [
+                'default' => null,
+                'limit' => 40,
+                'null' => false,
+            ])
+            ->addColumn('data', 'binary', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('expires', 'integer', [
+                'default' => null,
+                'limit' => 10,
+                'null' => true,
+            ])
+            ->addColumn('created', 'timestamp', [
+                'timezone' => true,
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'precision' => 6,
+                'scale' => 6,
+            ])
+            ->addColumn('modified', 'timestamp', [
+                'timezone' => true,
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'precision' => 6,
+                'scale' => 6,
+            ])
+            ->create();
+    }
+}
