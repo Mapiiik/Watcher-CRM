7937539491adcd5d89c7da9424a96d80caf809d8
Martin Patocka <mapik@mapik.net>
2022-06-01 11:11:58 +0000

    Labels - index - fix missing - usage count

--- templates/Labels/index.php
+++ templates/Labels/index.php
@@ -30,6 +30,7 @@
                     <td style="background-color: <?= h($label->color) ?>;"><?= h($label->color) ?></td>
                     <td><?= h($label->validity) ?></td>
                     <td><?= $label->dynamic ? __('Yes') : __('No'); ?></td>
+                    <td><?= $this->Number->format(count($label->customer_labels)) ?></td>
                     <td class="actions">
                         <?= $this->AuthLink->link(__('View'), ['action' => 'view', $label->id]) ?>
                         <?= $this->AuthLink->link(
