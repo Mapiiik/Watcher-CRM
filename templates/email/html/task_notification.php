b4fce3de4b2f464656e320ba11cdc5a2122913d6
Martin Patocka <mapik@mapik.net>
2022-11-11 07:47:26 +0100

    Cancel splitting task notification email template

--- templates/email/html/task_notification.php
+++ templates/email/html/task_notification.php
@@ -58,8 +58,6 @@ table {
                     <th><?= __('Dealer') ?></th>
                     <td><?= $task->has('dealer') ? h($task->dealer->name) : '' ?></td>
                 </tr>
-            </table>
-            <table>
                 <tr>
                     <th><?= __('Subject') ?></th>
                     <td><?= h($task->subject) ?></td>
@@ -107,8 +105,6 @@ table {
                     <th><?= __('Finish Date') ?></th>
                     <td><?= h($task->finish_date) ?></td>
                 </tr>
-            </table>
-            <table>
                 <tr>
                     <th><?= __('Id') ?></th>
                     <td><?= $this->Number->format($task->id) ?></td>
