42f398ab56f52244d31255dcd9d30e2b97bfa5f9
Martin Patocka <mapik@mapik.net>
2022-12-23 14:14:23 +0000

    Contract - print - notice "signed" in filename

--- templates/Contracts/pdf/print.php
+++ templates/Contracts/pdf/print.php
@@ -27,28 +27,36 @@ switch ($type) {
         $pdf = new ContractPDF('P', 'mm', 'A4');
         $pdf->generateContract($contract, $contract_version, $type, $signed);
         $pdf->Output($contract->number . '_' . $type . '_'
-            . $contract_version->valid_from->i18nFormat('yyyy-MM-dd') . '.pdf', 'I');
+            . $contract_version->valid_from->i18nFormat('yyyy-MM-dd')
+            . ($signed ? '-signed' : '')
+            . '.pdf', 'I');
         break;
     case 'contract-termination':
         //Generate PDF
         $pdf = new ContractPDF('P', 'mm', 'A4');
         $pdf->generateContract($contract, $contract_version, $type, $signed);
         $pdf->Output($contract->number . '_' . $type . '_'
-            . $contract_version->valid_until->i18nFormat('yyyy-MM-dd') . '.pdf', 'I');
+            . $contract_version->valid_until->i18nFormat('yyyy-MM-dd')
+            . ($signed ? '-signed' : '')
+            . '.pdf', 'I');
         break;
     case 'handover-protocol-installation':
         //Generate PDF
         $pdf = new ContractPDF('P', 'mm', 'A4');
         $pdf->generateHandoverProtocol($contract, $contract_version, $type, $signed, $technical_details);
         $pdf->Output($contract->number . '_' . $type . '_'
-            . $contract_version->valid_from->i18nFormat('yyyy-MM-dd') . '.pdf', 'I');
+            . $contract_version->valid_from->i18nFormat('yyyy-MM-dd')
+            . ($signed ? '-signed' : '')
+            . '.pdf', 'I');
         break;
     case 'handover-protocol-uninstallation':
         //Generate PDF
         $pdf = new ContractPDF('P', 'mm', 'A4');
         $pdf->generateHandoverProtocol($contract, $contract_version, $type, $signed);
         $pdf->Output($contract->number . '_' . $type . '_'
-            . $contract_version->valid_until->i18nFormat('yyyy-MM-dd') . '.pdf', 'I');
+            . $contract_version->valid_until->i18nFormat('yyyy-MM-dd')
+            . ($signed ? '-signed' : '')
+            . '.pdf', 'I');
         break;
     default:
         exit;
