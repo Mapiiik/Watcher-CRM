422a9206648af504bddebc1e4bccc0192d88a297
Martin Patocka <mapik@mapik.net>
2023-07-03 22:28:47 +0000

    Fix long lines

--- templates/Customers/add.php
+++ templates/Customers/add.php
@@ -37,9 +37,15 @@
                         echo $this->Form->control('bank_account');
                         echo $this->Form->control('bank_code');
                         echo $this->Form->control('bank_name');
-                        echo $this->Form->control('tax_rate_id', ['options' => $taxRates]);
-                        echo $this->Form->control('dealer', ['options' => $customer->getDealerStateOptions()]);
-                        echo $this->Form->control('invoice_delivery_type', ['options' => $customer->getInvoiceDeliveryTypeOptions()]);
+                        echo $this->Form->control('tax_rate_id', [
+                            'options' => $taxRates,
+                        ]);
+                        echo $this->Form->control('dealer', [
+                            'options' => $customer->getDealerStateOptions(),
+                        ]);
+                        echo $this->Form->control('invoice_delivery_type', [
+                            'options' => $customer->getInvoiceDeliveryTypeOptions(),
+                        ]);
                         echo $this->Form->control('agree_gdpr', [
                             'label' => __('Agrees to Processing of Personal Data'),
                         ]);
