22ead4d2a058f9851b6ac00087cc84174c2ab914
Martin Patocka <mapik@mapik.net>
2023-07-03 12:03:56 +0000

    Update docComment for Application

--- src/Application.php
+++ src/Application.php
@@ -38,6 +38,8 @@ use CakeDC\Users\Plugin;
  *
  * This defines the bootstrapping logic and middleware layers you
  * want to use in your application.
+ *
+ * @extends \Cake\Http\BaseApplication<\App\Application>
  */
 class Application extends BaseApplication
 {
