dc8d70bfaa6c273367397de051df1b5550743b92
Martin Patocka <mapik@mapik.net>
2023-08-26 18:00:46 +0000

    Completed QueueMailerTest

--- tests/TestCase/Mailer/QueueMailerTest.php
+++ tests/TestCase/Mailer/QueueMailerTest.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace App\Test\TestCase\Mailer;
 
+use App\Mailer\QueueMailer;
+use Cake\I18n\Date;
+use Cake\TestSuite\EmailTrait;
 use Cake\TestSuite\TestCase;
 
 /**
@@ -10,10 +13,58 @@ use Cake\TestSuite\TestCase;
  */
 class QueueMailerTest extends TestCase
 {
+    use EmailTrait;
+
     /**
      * Test subject
      *
      * @var \App\Mailer\QueueMailer
      */
     protected $Queue;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->loadRoutes();
+    }
+
+    /**
+     * Test index method
+     *
+     * @return void
+     * @uses \App\Controller\TaxRatesController::index()
+     */
+    public function testServiceChange(): void
+    {
+        $mailer = new QueueMailer('contracts');
+        $mailer->send(
+            'serviceChange',
+            [
+                ['test@test.test'],
+                [
+                    'customer_name' => 'Bc. Martin Patočka',
+                    'contract_number' => '000000-0000',
+                    'installation_address' => 'Somewhere in the middle of nowhere',
+                    'original_billing_name' => 'Original service name',
+                    'original_billing_sum' => 111.11,
+                    'new_billing_name' => 'New service name',
+                    'new_billing_sum' => 222.22,
+                    'new_billing_from' => h(new Date()),
+                ],
+            ]
+        );
+
+        $this->assertMailSentTo('test@test.test');
+        $this->assertMailContainsText('000000-0000');
+        $this->assertMailContainsText('Somewhere in the middle of nowhere');
+        $this->assertMailContainsText('Original service name');
+        $this->assertMailContainsText('111');
+        $this->assertMailContainsText('New service name');
+        $this->assertMailContainsText('222');
+    }
 }
