a222ced063b9f3a7f37ffcb09b23ad2341f51920
Martin Patocka <mapik@mapik.net>
2023-08-28 07:46:04 +0000

    Emails - service change - updated text

--- templates/email/text/service_change.php
+++ templates/email/text/service_change.php
@@ -17,7 +17,7 @@
 ?>
 Vážený zákazníku,
 
-více než 10 let se nám dařilo držet naše ceny připojení k internetu stále stejné, ale bohužel z důvodu současné ekonomické situace, kdy se neustále navyšují náklady na provoz, jsme byli nuceni pro Vás připravit nové tarify s vyššími rychlostmi, ale i mírným navýšením ceny.
+více než 15 let se nám dařilo držet naše ceny připojení k internetu stále stejné, ale bohužel z důvodu současné ekonomické situace, kdy se neustále navyšují náklady na provoz, jsme byli nuceni pro Vás připravit nové tarify s mírným navýšením ceny, ale i vyššími rychlostmi.
 
 Od <?= $data['new_billing_from'] ?> bude na Vaší přípojce č. <?= $data['contract_number'] ?><?= $data['installation_address'] ? ', na adrese ' . $data['installation_address'] : '' ?>
 
@@ -25,11 +25,11 @@ změněn stávající tarif <?= $data['original_billing_name'] ?> za <?= $this->
 
 na nový tarif <?= $data['new_billing_name'] ?> za <?= $this->Number->currency($data['new_billing_sum']) ?>.
 
-Nový ceník platný od 01.10.2023 je uveřejněny na našich internetových stránkách zde: https://netair.cz/cenik-pripojeni/
+Nový ceník platný od 01.10.2023 je uveřejněný na našich internetových stránkách zde: https://netair.cz/cenik-pripojeni/
 
 V souladu s ust. § 63 odst. 6 zákona č. 127/2005 Sb. o elektronických komunikacích Vás také informujeme, že jestliže tuto změnu neakceptujete, jste oprávněn smlouvu s naší společností bez sankce vypovědět k datu nabytí účinnosti této změny.
 
-Pokud budete mít jakékoliv dotazy neváhejte nás kontaktovat
+Pokud budete mít jakékoliv dotazy neváhejte nás kontaktovat.
 
 NETAIR, s.r.o.
 512 43 Jablonec nad Jizerou, č.p. 299
