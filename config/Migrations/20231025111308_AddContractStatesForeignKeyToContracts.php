3c8c8da5303608f15f6892bf581e4ef55454ec88
Martin Patocka <mapik@mapik.net>
2023-10-25 11:39:16 +0000

    Add ContractStates foreign key to Contracts

---
+++ config/Migrations/20231025111308_AddContractStatesForeignKeyToContracts.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class AddContractStatesForeignKeyToContracts extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * @return void
+     */
+    public function change(): void
+    {
+        $table = $this->table('contracts');
+
+        $table->addForeignKey('contract_state_id', 'contract_states', 'id', [
+            'delete' => 'NO_ACTION',
+            'update' => 'NO_ACTION',
+        ]);
+
+        $table->update();
+    }
+}
