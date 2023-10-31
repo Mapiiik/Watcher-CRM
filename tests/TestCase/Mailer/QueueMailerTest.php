4d7a4dee8d4d3d55afdc85cbb3a85d0fcc2d9669
Martin Patocka <mapik@mapik.net>
2023-10-31 12:25:00 +0000

    Fix mailer tests

--- tests/TestCase/Mailer/QueueMailerTest.php
+++ tests/TestCase/Mailer/QueueMailerTest.php
@@ -52,8 +52,16 @@ class QueueMailerTest extends TestCase
                     'installation_address' => 'Somewhere in the middle of nowhere',
                     'original_billing_name' => 'Original service name',
                     'original_billing_sum' => 111.11,
+                    'original_billing_percentage_discount' => null,
+                    'original_billing_percentage_discount_sum' => 0.00,
+                    'original_billing_fixed_discount_sum' => 11.01,
+                    'original_billing_total_price' => 100.10,
                     'new_billing_name' => 'New service name',
                     'new_billing_sum' => 222.22,
+                    'new_billing_percentage_discount' => null,
+                    'new_billing_percentage_discount_sum' => 0.00,
+                    'new_billing_fixed_discount_sum' => 22.02,
+                    'new_billing_total_price' => 200.20,
                     'new_billing_from' => h(new Date()),
                 ],
             ]
@@ -63,8 +71,9 @@ class QueueMailerTest extends TestCase
         $this->assertMailContainsText('000000-0000');
         $this->assertMailContainsText('Somewhere in the middle of nowhere');
         $this->assertMailContainsText('Original service name');
-        $this->assertMailContainsText('111');
+        $this->assertMailContainsText('100');
         $this->assertMailContainsText('New service name');
+        $this->assertMailContainsText('200');
         $this->assertMailContainsText('222');
     }
 }
