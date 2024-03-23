9274e6aeaacd67bd4a8ddfdb4fae6174f4518f38
Martin Patocka <mapik@mapik.net>
2024-03-23 16:23:55 +0100

    Fix AJAX View

--- src/View/AjaxView.php
+++ src/View/AjaxView.php
@@ -11,36 +11,30 @@ declare(strict_types=1);
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  * @link          https://cakephp.org CakePHP(tm) Project
- * @since         3.0.4
+ * @since         3.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace App\View;
 
 /**
  * A view class that is used for AJAX responses.
- * Currently only switches the default layout and sets the response type -
- * which just maps to text/html by default.
+ * Currently, only switches the default layout and sets the response type - which just maps to
+ * text/html by default.
  */
 class AjaxView extends AppView
 {
     /**
-     * The name of the layout file to render the view inside of. The name
-     * specified is the filename of the layout in /templates/Layout without
-     * the .php extension.
-     *
-     * @var string
+     * @inheritDoc
      */
     protected string $layout = 'ajax';
 
     /**
-     * Initialization hook method.
+     * Get content type for this view.
      *
-     * @return void
+     * @return string
      */
-    public function initialize(): void
+    public static function contentType(): string
     {
-        parent::initialize();
-
-        $this->response = $this->response->withType('ajax');
+        return 'text/html';
     }
 }
