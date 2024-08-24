6e04235bc9dbe21915cc86b4ddf65ed758ad317b
Martin Patocka <mapik@mapik.net>
2024-08-24 08:28:51 +0200

    figuration to sync with cakephp/app - fix CS

--- config/app.php
+++ config/app.php
@@ -481,7 +481,7 @@ return [
     'DebugKit' => [
         'forceEnable' => filter_var(env('DEBUG_KIT_FORCE_ENABLE', false), FILTER_VALIDATE_BOOLEAN),
         'safeTld' => env('DEBUG_KIT_SAFE_TLD', null),
-        'ignoreAuthorization' => env('DEBUG_KIT_IGNORE_AUTHORIZATION', false)
+        'ignoreAuthorization' => env('DEBUG_KIT_IGNORE_AUTHORIZATION', false),
     ],
 
     /*
