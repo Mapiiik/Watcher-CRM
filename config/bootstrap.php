201b5ea6efede22c9bcd5ba3db85f18fcabbed3d
Martin Patocka <mapik@mapik.net>
2024-09-22 10:24:28 +0000

    replace _cake_core_ with _cake_translations_ cache config

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -103,7 +103,7 @@ if (file_exists(CONFIG . 'app_local.php')) {
  */
 if (Configure::read('debug')) {
     Configure::write('Cache._cake_model_.duration', '+2 minutes');
-    Configure::write('Cache._cake_core_.duration', '+2 minutes');
+    Configure::write('Cache._cake_translations_.duration', '+2 minutes');
 }
 
 /*
--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -155,7 +155,7 @@ endif;
                             <li class="bullet problem">Your logs directory is NOT writable.</li>
                         <?php endif; ?>
 
-                        <?php $settings = Cache::getConfig('_cake_core_'); ?>
+                        <?php $settings = Cache::getConfig('_cake_translations_'); ?>
                         <?php if (!empty($settings)) : ?>
                             <li class="bullet success">The <em><?= h($settings['className']) ?></em> is being used for core caching. To change the config edit config/app.php</li>
                         <?php else : ?>
