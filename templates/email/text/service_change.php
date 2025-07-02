f2e83e8af6093a655bbbc72a8ff06b799f0297d5
Martin Patocka <mapik@mapik.net>
2025-07-02 10:41:36 +0200

    Bulk Service Change - updated texts

--- templates/email/text/service_change.php
+++ templates/email/text/service_change.php
@@ -10,16 +10,7 @@ $billing_date = (new Date((string)$data['new_billing_from']))->lastOfMonth();
 ?>
 Vážený zákazníku,
 
-více než 15 let se nám dařilo držet naše ceny připojení k internetu stále stejné, ale bohužel z důvodu současné ekonomické situace, kdy se neustále navyšují náklady na provoz, jsme byli nuceni pro Vás připravit nové tarify.
-
-<?php if (
-    ($data['original_billing_sum'] > $data['original_billing_total_price'])
-    && ($data['new_billing_sum'] > $data['new_billing_total_price'])
-) : ?>
-Vaše přípojka měla historicky nastavenou zvýhodněnou sazbu. Proto i nový tarif, bude pro Vás zvýhodněn, oproti standardní ceně.
-
-<?php endif; ?>
-Od <?= $data['new_billing_from'] ?> bude na Vaší přípojce č. <?= $data['contract_number'] ?><?= $data['installation_address'] ? ', na adrese ' . $data['installation_address'] : '' ?>
+od <?= $data['new_billing_from'] ?> bude na Vaší přípojce č. <?= $data['contract_number'] ?><?= $data['installation_address'] ? ', na adrese ' . $data['installation_address'] : '' ?>
 
 
 změněn stávající tarif:
@@ -36,9 +27,17 @@ na nový tarif:
     <?= $data['new_billing_name'] ?> za cenu <?= $this->Number->currency($data['new_billing_total_price']) ?> měsíčně
 <?php endif; ?>
 
+<?php if (
+    ($data['original_billing_sum'] > $data['original_billing_total_price'])
+    && ($data['new_billing_sum'] > $data['new_billing_total_price'])
+) : ?>
+Vaše přípojka měla historicky nastavenou zvýhodněnou sazbu. Proto i nový tarif, bude pro Vás zvýhodněn, oproti standardní ceně.
+
+<?php endif; ?>
+
 Jelikož se fakturuje zpětně, první platba za nový tarif by měla proběhnout v období od <?= $billing_date ?> do <?= $billing_date->addDays(10) ?>.
 
-Nový ceník platný od 01.10.2023 je uveřejněný na našich internetových stránkách zde: https://netair.cz/cenik-pripojeni/
+Aktuálně platný ceník je uveřejněný na našich internetových stránkách zde: https://netair.cz/cenik-pripojeni/
 
 V souladu s ust. § 63 odst. 6 zákona č. 127/2005 Sb. o elektronických komunikacích Vás také informujeme, že jestliže tuto změnu neakceptujete, jste oprávněn smlouvu s naší společností bez sankce vypovědět k datu nabytí účinnosti této změny.
 
