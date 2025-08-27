2b7f39e22c6d228a607f1c16eefb90f88319f11c
Martin Patocka <mapik@mapik.net>
2025-08-27 13:37:52 +0000

    Migrations - UsersSeed - let's set up IO just to be sure

--- config/Migrations/20211105114150_InitialCRM.php
+++ config/Migrations/20211105114150_InitialCRM.php
@@ -1302,6 +1302,7 @@ class InitialCRM extends BaseMigration
 
         (new UsersSeed())
             ->setAdapter($this->getAdapter())
+            ->setIo($this->getIo())
             ->run();
     }
 
