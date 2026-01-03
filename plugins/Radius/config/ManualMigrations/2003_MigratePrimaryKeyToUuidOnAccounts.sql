19f07fb66d256c636f0d407b58483662c2bd4323
Martin Patocka <mapik@mapik.net>
2026-01-03 17:08:46 +0000

    Docs - RADIUS - Clarify Bucardo resync steps after accounts PK migration

--- plugins/Radius/config/ManualMigrations/2003_MigratePrimaryKeyToUuidOnAccounts.sql
+++ plugins/Radius/config/ManualMigrations/2003_MigratePrimaryKeyToUuidOnAccounts.sql
@@ -1,5 +1,29 @@
--- Run this on slave DB, after primary DB is migrated to 20231029090040_MigratePrimaryKeyToUuidOnAccounts
--- (You will probably need to manually resync the table from the master to the slave DB and if you are using Bucardo you will need to re-set the sync due to the change in primary key type)
+-- Run this on the SLAVE DB after the primary DB has been migrated
+-- using migration 20231029090040_MigratePrimaryKeyToUuidOnAccounts.
+--
+-- Due to the primary key type change, the table will most likely need
+-- to be manually resynced from the master DB. If Bucardo is used,
+-- the sync must be reconfigured.
+
+-- Example Bucardo resync procedure:
+-- bucardo stop sync radius
+-- bucardo del table public.accounts
+-- bucardo del sequence public.accounts_id_seq
+
+-- To be absolutely sure (run on BOTH databases if the table was previously
+-- part of a Bucardo sync and the primary key type was changed):
+-- DROP TRIGGER bucardo_delta ON public.accounts;
+-- DROP FUNCTION bucardo.delta_public_accounts();
+-- DROP TABLE IF EXISTS bucardo.delta_public_accounts;
+-- TRUNCATE TABLE public.accounts;
+-- DROP SEQUENCE IF EXISTS public.accounts_id_seq;
+
+-- Re-add the table to Bucardo:
+-- bucardo add table public.accounts relgroup=radius
+-- bucardo start sync radius
+--
+-- If the sync does not kick in automatically, perform a manual
+-- export → truncate → import of the accounts table on the PRIMARY DB.
 
 BEGIN;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
