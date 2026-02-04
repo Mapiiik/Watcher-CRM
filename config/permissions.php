8f8c3e5e1ad06873dcba9e662da95ce6d65584de
Martin Patocka <mapik@mapik.net>
2026-02-04 13:04:41 +0000

    PHPStan - narrow ignore to include.fileNotFound

--- config/permissions.php
+++ config/permissions.php
@@ -478,7 +478,7 @@ $permissions = [
  */
 $localPermissionsFile = CONFIG . 'permissions_local.php';
 if (file_exists($localPermissionsFile)) {
-    /** @phpstan-ignore-next-line */
+    /** @phpstan-ignore-next-line include.fileNotFound */
     $localPermissions = include $localPermissionsFile;
     /** @psalm-suppress RedundantCondition */
     if (is_array($localPermissions)) {
